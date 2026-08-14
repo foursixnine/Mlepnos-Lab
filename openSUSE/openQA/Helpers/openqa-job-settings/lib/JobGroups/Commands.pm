0b46249895d53a34162249483232cd86152838d1
Santiago Zarate <santiago@zarate.co>
2026-08-14 17:36:56 +0200

    Update bump command to latest working state
    (cherry picked from commit eb699cac0e8de44ea432d28b61eb5845acbbd652)

--- openSUSE/openQA/Helpers/openqa-job-settings/lib/JobGroups/Commands.pm
+++ openSUSE/openQA/Helpers/openqa-job-settings/lib/JobGroups/Commands.pm
@@ -2,7 +2,11 @@
 package JobGroups::Commands;
 use Mojo::Base 'Mojolicious::Commands', -signatures;
 
+# subcommand classes live under JobGroups::Command::
 has namespaces => sub { ['JobGroups::Command'] };
+
 has message => 'JobGroups CLI - commands for working with openQA products';
+
+$SIG{__DIE__} = \&Carp::confess;
 
 1;
